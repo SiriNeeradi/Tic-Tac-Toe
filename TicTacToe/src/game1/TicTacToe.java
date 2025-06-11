47831f6abfa2a8a50d10b69f9069f573875d2a8c
SiriNeeradi <sirineeradi07@gmail.com>
2025-06-11 22:32:56 +0530

    Initialized the Board for TicTacToe Game.

--- TicTacToe/src/game1/TicTacToe.java
+++ TicTacToe/src/game1/TicTacToe.java
@@ -1,9 +1,33 @@
 package game1;
-
+import java.util.*;
 public class TicTacToe {
-
+	
+	//3X3 board which should take char input either X or O
+	static char board[][] = {
+			{' ',' ',' '},
+			{' ',' ',' '},
+			{' ',' ',' '}
+	};
+	
 	public static void main(String[] args) {
-		System.out.println("Hello Siri");
+		
+		Scanner sc = new Scanner(System.in);
+		char currentPlayer = 'X'; 		//Player1 = X
+		boolean gameStarted = true;		//Game initializer
+		printboard();
+		
 	}
-
+	
+	//For printing the board when players take turns
+	public static void printboard() {
+		System.out.println(" ----------");
+		for(int i = 0; i <= 2; i++) {
+			for(int j = 0; j <= 3; j++){
+			    System.out.print(" | ");
+			}
+			System.out.println();
+			System.out.println(" ----------");
+		}
+	}
+	
 }
