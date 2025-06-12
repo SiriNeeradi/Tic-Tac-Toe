8875267232b5d7c82f4acdc35f1220b710a3fe15
SiriNeeradi <sirineeradi07@gmail.com>
2025-06-12 21:38:17 +0530

    Added player haswon method.

--- TicTacToe/src/game1/TicTacToe.java
+++ TicTacToe/src/game1/TicTacToe.java
@@ -14,12 +14,14 @@ public class TicTacToe {
 		Scanner sc = new Scanner(System.in);
 		char currentPlayer = 'X'; 		//Player1 = X
 		boolean gameStarted = true;		//Game initializer
-		printboard();
+		System.out.println("Welcome to tic tac toe!");
+		printBoard();
+		
 		
 	}
 	
 	//For printing the board when players take turns
-	public static void printboard() {
+	public static void printBoard() {
 		System.out.println(" ----------");
 		for(int i = 0; i <= 2; i++) {
 			for(int j = 0; j <= 3; j++){
@@ -29,5 +31,23 @@ public class TicTacToe {
 			System.out.println(" ----------");
 		}
 	}
+	
+	//Winning cases for a player
+	public static boolean hasWon(char player) {
+		for(int i = 0; i < 3; i++) {
+			if(board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+				return true;
+			}
+			else if(board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+				return true;
+			}
+		}
+		if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+            return true;
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+            return true;
+        return false;
+	}
+	
 	
 }
